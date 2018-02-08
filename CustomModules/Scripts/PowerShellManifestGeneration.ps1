e714047788021a96f841ddc14b4239e550a15d57
Arsen <timchuk201092@gmail.com>
2018-02-08 11:45:10 +0200

    Add PowerShell manifest generation

---
+++ CustomModules/Scripts/PowerShellManifestGeneration.ps1
@@ -0,0 +1,10 @@
+﻿$manifest = @{
+	Path = "D:\Code\PowerShellCmdlets\CustomModules\bin\Release\CustomModules.psd1"
+	RootModule = "CustomModules"
+	CompanyName = "Ciklum"
+	Author = "Ciklum"
+	ModuleVersion = "1.0.0"
+	Description = "Custom Get-MVSwitch Powershell cmdlet to getting virtual machine switches."
+}
+
+New-ModuleManifest @manifest
