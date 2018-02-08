c9cc5d9f254269ae4013b84a1a44456611b88534
Arsen <timchuk201092@gmail.com>
2018-02-08 11:43:20 +0200

    Add checking session policy

---
+++ CustomModules/Helpers/PrincipalHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomModules.Helpers
+{
+    using System.Linq;
+    using System.Management.Automation;
+
+    internal static class PrincipalHelper
+    {
+        internal static bool IsAdminRole
+        {
+            get
+            {
+                using (var powershell = PowerShell.Create())
+                {
+                    const string script = 
+                        @"$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+                          $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)";
+
+                    powershell.AddScript(script);
+
+                    var result = powershell.Invoke().First();
+                    
+                    return (bool) result.BaseObject;
+                }
+            }
+        }
+    }
+}
